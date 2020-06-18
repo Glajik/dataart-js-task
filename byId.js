c2fb2c2f2ecec25c446dff6224f3942f2c97ecdd
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-06-19 02:58:35 +0300

    Write function

--- byId.js
+++ byId.js
@@ -1,5 +1,12 @@
-function byId(items) {
+function transform(acc, item) {
+  return Object.assign({ [item.id]: item }, acc);
+}
 
+function byId(items) {
+  if (!items.length) {
+    return {};
+  }
+  return items.reduce(transform, {});
 }
 
 exports.default = byId
